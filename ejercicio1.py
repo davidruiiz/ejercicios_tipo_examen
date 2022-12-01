d4b708c898d3c921841bbe57b51f9f77b700405e
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-12-01 11:02:56 +0100

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,37 @@
+"""
+EJERCICIO SUMA SIMPLE DE UNA MATRIZ
+Dada una matriz de números enteros N, encuentre la suma de sus elementos.
+Función descriptiva
+Complete la función simpleArraySum . Debe devolver la suma de los elementos de la matriz como un número entero.
+simpleArraySum tiene los siguientes parámetros:
+ar : una matriz de números enteros
+Formato de entrada
+La primera línea contiene un número entero, N, que denota el tamaño de la matriz.
+La segunda línea contiene N enteros separados por espacios que representan los elementos de la matriz.
+Código Inicial
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the simpleArraySum function below.
+def simpleArraySum(ar):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+    
+    fptr.close()
